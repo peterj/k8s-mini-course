19589b287db6848724cc7940eb05cdb4cca8854c
Peter Jausovec <peter.jausovec@gmail.com>
2020-12-01 19:52:18 -0800

    Add a website icon and dynamically load the links.

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -1,47 +1,51 @@
-import { social } from "@config";
-import { Facebook, Twitch, Twitter, YouTube } from "@icons/index";
-import classNames from "classnames";
-import React from "react";
+import { social } from '@config';
+import { Facebook, Twitch, Twitter, YouTube, Website } from '@icons/index';
+import classNames from 'classnames';
+import React from 'react';
 
 const Icons = {
   facebook: <Facebook />,
   twitter: <Twitter />,
   youtube: <YouTube />,
   twitch: <Twitch />,
+  website: <Website />,
 };
 
 const SocialLink = ({ type }) => {
   const btnClass = classNames({
     [`flex text-center text-gray-300 p-2 transition-colors duration-300 hover:text-white fill-current`]: true,
-    [`hover:bg-facebook`]: type === "facebook",
-    [`hover:bg-twitter`]: type === "twitter",
-    [`hover:bg-twitch`]: type === "twitch",
-    [`hover:bg-youtube`]: type === "youtube",
+    [`hover:bg-facebook`]: type === 'facebook',
+    [`hover:bg-twitter`]: type === 'twitter',
+    [`hover:bg-twitch`]: type === 'twitch',
+    [`hover:bg-youtube`]: type === 'youtube',
+    [`hover:bg-green-500`]: type === 'website',
   });
   return (
-    <li className="sm:w-auto text-center m-0 self-center">
+    <li className='self-center m-0 text-center sm:w-auto'>
       <a
-        target="_blank"
+        target='_blank'
         title={`Follow me on ${type.toUpperCase()}`}
         href={social[type]}
-        rel="noopener"
+        rel='noopener'
         className={btnClass}
       >
-        <span className="w-4 h-4 inline-block">{Icons[type]}</span>
+        <span className='inline-block w-4 h-4'>{Icons[type]}</span>
       </a>
     </li>
   );
 };
 
-const SocialIcons = () => (
-  <div className="sm:ml-auto w-full mt-2">
-    <ul className="flex m-0">
-      <SocialLink type="twitter" />
-      <SocialLink type="facebook" />
-      <SocialLink type="youtube" />
-      <SocialLink type="twitch" />
-    </ul>
-  </div>
-);
+const SocialIcons = () => {
+  Object.keys(social).map((s) => console.log(s));
+  return (
+    <div className='w-full mt-2 sm:ml-auto'>
+      <ul className='flex m-0'>
+        {Object.keys(social).map((s) => (
+          <SocialLink key={s} type={s} />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default SocialIcons;
